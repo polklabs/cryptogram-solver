5bf17858c9b605e4d139b5d1c48533df448f054d
Andrew Polk <polkandrew01@gmail.com>
2022-01-19 18:24:31 -0800

    Added new word index option. Changed dictionary format More puzzles.

--- Decoder.py
+++ Decoder.py
@@ -1,68 +1,40 @@
-from operator import le
 import re
 
 checks = 0
-highestLevel = 0
 bestGuess = ""
 
 def testWord(cypher, puzzle):
     return "".join([cypher[c] for c in puzzle])
 
-# print("> Loading Words")
-# wordDict = dict() #{ [length]: words[] }
+print("> Loading Words")
+wordDict = dict() #{ [length]: words[] }
 
-# def loadDictionary(filename):
-#     with open(filename, 'r') as f:
-#         wordList = f.read().splitlines()
-#         for word in wordList:
-#             wLength = len(word)
-#             if wLength not in wordDict:
-#                 wordDict[wLength] = dict()
-#             wordDict[wLength][word] = 1
+def loadDictionary(filename, skip=0):
+    with open(filename, 'r') as f:
+        for _ in range(skip):
+            f.readline()
+        wordList = f.read().splitlines()
+        for word in wordList:
+            word = word.split()[0].lower()
+            wLength = len(word)
+            if wLength not in wordDict:
+                wordDict[wLength] = dict()
+            wordDict[wLength][word] = 1
 
-# # Load common words first so they're searched first
+# Load common words first so they're searched first
 # loadDictionary('words_common.txt')
 # loadDictionary('dictionary.txt')
+loadDictionary('SUBTLEXus74286wordstextversion.txt', 1)
 
-# for key in wordDict.keys():
-#     wordDict[key] = list(wordDict[key])
-
-print("> Load Words w/ Frequency")
-wordList = []
-with open('SUBTLEXus74286wordstextversion.txt', 'r') as f:
-    f.readline() #Skip first line
-    lines = f.read().splitlines()
-    words = [w.split()[0].lower() for w in lines]
-    wordList = [words[:742], words[742:7428], words[7428:]]
-for i in range(len(wordList)):
-    wordDict = dict()
-    for word in wordList[i]:
-        wLength = len(word)
-        if wLength not in wordDict:
-            wordDict[wLength] = dict()
-        wordDict[wLength][word] = 1
-    for key in wordDict.keys():
-        wordDict[key] = list(wordDict[key])
-    wordList[i] = wordDict
-
-# with open('words_alpha.txt', 'r') as f:
-#     words = f.read().splitlines()
-#     wordDict = dict()
-#     for word in words:
-#         wLength = len(word)
-#         if wLength not in wordDict:
-#             wordDict[wLength] = dict()
-#         wordDict[wLength][word] = 1
-#     for key in wordDict.keys():
-#         wordDict[key] = list(wordDict[key])
-#     wordList.append(wordDict)
+for key in wordDict.keys():
+    wordDict[key] = list(wordDict[key])
 
 print("> Loading Puzzle")
 #Load Puzzle
 puzzle = ""
 solution = ""
 hasSolution = False
-with open('puzzle83.txt', 'r') as f:
+with open('pz.txt', 'r') as f:
     lines = f.read().splitlines()
     puzzle = lines[0]
     bestGuess = "_"*len(puzzle)
@@ -109,9 +81,21 @@ def getWordIndex(outputWords, wordIndex, type="next"):
                     index = i
         return index
 
-def getPossibleWords(finalWord, length, level=0):
+    # Word with smallest ratio of _ in it
+    if type == "ratio":
+        count = 100
+        index = 0
+        for i in range(len(outputWords)):
+            if '_' in outputWords[i]:
+                ratio = outputWords[i].count('_') / len(outputWords[i]) * 100
+                if ratio < count:
+                    count = ratio
+                    index = i
+        return index
+
+def getPossibleWords(finalWord, length):
     # Get all words of same length
-    possibleWords = wordList[level][length]
+    possibleWords = wordDict[length]
 
     # Filter by known mappings
     regex = re.compile(finalWord.replace("_", "."))
@@ -120,7 +104,7 @@ def getPossibleWords(finalWord, length, level=0):
     return possibleWords
 
 def Solve(cypher, puzzle, wordIndex=0):
-    global checks, highestLevel, bestGuess
+    global checks, bestGuess, wordDict
     checks += 1
 
     output = testWord(cypher, puzzle)
@@ -140,7 +124,7 @@ def Solve(cypher, puzzle, wordIndex=0):
             goOn = input("Keep Looking [Y/n]: ")
             return goOn.lower() == "n"
     
-    index = getWordIndex(finalWords, wordIndex, "largest")
+    index = getWordIndex(finalWords, wordIndex, "ratio")
     missingWord = puzzleWords[index]
     finalWord = finalWords[index]
     wordLength = len(missingWord)
@@ -154,10 +138,9 @@ def Solve(cypher, puzzle, wordIndex=0):
     for word in finalWords:
         if "_" not in word:
             missing = True
-            for wordDict in wordList:
-                if len(word) in wordDict:
-                    if word in wordDict[len(word)]:
-                        missing = False
+            if len(word) in wordDict:
+                if word in wordDict[len(word)]:
+                    missing = False
             if missing:
                 return False
 
@@ -165,36 +148,32 @@ def Solve(cypher, puzzle, wordIndex=0):
         if "_" in word:
             missing = True
             incompleteRegex = re.compile(word.replace("_", "."))
-            for wordDict in wordList:
-                if len(word) in wordDict:
-                    for w in wordDict[len(word)]:
-                        if bool(re.match(incompleteRegex, w)):
-                            missing = False
-                            break
+            if len(word) in wordDict:
+                for w in wordDict[len(word)]:
+                    if bool(re.match(incompleteRegex, w)):
+                        missing = False
+                        break
             if missing:
                 return False
     
-    for i in range(len(wordList)):
-        possibleWords = getPossibleWords(finalWord, wordLength, i)
-        highestLevel = max(highestLevel, i)
+    possibleWords = getPossibleWords(finalWord, wordLength)
 
-        for possibleWord in possibleWords:
-            # Assign mappings
-            newCypher = cypher.copy()
-            for i in range(len(possibleWord)):
-                char = possibleWord[i]
-                newCypher[missingWord[i]] = char
+    for possibleWord in possibleWords:
+        # Assign mappings
+        newCypher = cypher.copy()
+        for i in range(len(possibleWord)):
+            char = possibleWord[i]
+            newCypher[missingWord[i]] = char
 
-                #Make sure all mappings are 1 to 1
-                if list(newCypher.values()).count(char) > 1:
-                    break
-            else:
-                # Next word
-                if Solve(newCypher, puzzle, wordIndex+1):
-                    return True
+            #Make sure all mappings are 1 to 1
+            if list(newCypher.values()).count(char) > 1:
+                break
+        else:
+            # Next word
+            if Solve(newCypher, puzzle, wordIndex+1):
+                return True
     return False
 
 print("> Solving Puzzle")
 Solve(cypher, puzzle)
 print(checks)
-print(highestLevel)
